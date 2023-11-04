0e992573e1eed7344e9a3c4e2b7775f405e456b5
sirithajohnny21 <sirithajohnny2155@gmail.com>
2023-11-04 11:24:16 +0530

    commit

---
+++ operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+int a,b;
+a = 21;
+b = 24;
+printf("a + b = %d\n", a+b );
+printf("a - b = %d\n", a-b );
+printf("a * b = %d\n", a*b );
+printf("a / b = %d\n", a/b );
+return 0;
+}
